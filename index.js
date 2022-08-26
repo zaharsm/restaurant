08127253437557571e61d0c934abc226a8960854
Zahar <zaharswork@gmail.com>
2022-08-26 15:56:42 +0530

    npm scripts deployment

--- index.js
+++ index.js
@@ -9,22 +9,38 @@ var span = document.getElementsByClassName("close")[0];
 var getOut = document.getElementById("exit");
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
-getOut.onclick = function() {
+getOut.onclick = function () {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
+
+$(document).ready(function () {
+  $("#mycarousel").carousel({ interval: 2000 });
+  $("#carouselButton").click(function () {
+    if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+      $("#mycarousel").carousel('pause');
+      $("#carouselButton").children("span").removeClass('fa-pause');
+      $("#carouselButton").children("span").addClass('fa-play');
+    }
+    else if ($("#carouselButton").children("span").hasClass('fa-play')) {
+      $("#mycarousel").carousel('cycle');
+      $("#carouselButton").children("span").removeClass('fa-play');
+      $("#carouselButton").children("span").addClass('fa-pause');
+    }
+  });
+});
